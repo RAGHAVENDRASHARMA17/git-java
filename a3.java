9256ac5d13b285aaf27c33af41123c475010b279
RAGHAVENDRASHARMA17 <raghavendrasharma2002@gmail.com>
2024-05-09 15:59:20 +0530

    Add files via upload

---
+++ a3.java
@@ -0,0 +1,17 @@
+class a3
+{
+	public static void main(String args[])
+	{
+	System.out.println("Infotech classes\n near arya-mobile");
+        System.out.println("pincode-305801");  
+	System.out.println("Kishangarh(ajmer),RAJASTHAN");
+	System.out.println("Phone - 9845984598");
+	}
+}
+
+
+/*
+class - java is truly object oriented language. everything should be inside a class, even the main function should also be inside a class
+public static void main(String args[]) - it is called the main fucntion of java (public - access specifier, static - storage class, void -non returning, main-name of the function, String args[] - command line arguments)
+System.out.println - java command to print anything on the screen
+*/
